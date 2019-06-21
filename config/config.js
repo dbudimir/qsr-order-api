122e262aed6d5f85d2795f12a5a4d70d6374c5c8
dbudimir <dav.budimir@gmail.com>
2019-06-21 02:34:56 -0400

    Updated passport config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,25 @@
+secretKey = makeid = () => {
+   var result = '';
+   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for (var i = 0; i < 10; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   // return result;
+}
+
+// console.log(makeid());
+
 module.exports = {
-   jwtSecret: 'JwtS3cr3tK3Y',
+   jwtSecret: 'secretKey',
    jwtSession: {
       session: false
    }
 }
+
+// module.exports = {
+//    jwtSecret: 'JwtS3cr3tK3Y',
+//    jwtSession: {
+//       session: false
+//    }
+// }
