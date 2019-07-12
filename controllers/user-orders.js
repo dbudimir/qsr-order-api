1287a610206b865b93ef03a5fcd507d1b34d362c
dbudimir <dav.budimir@gmail.com>
2019-07-11 20:33:51 -0400

    Updated peerdeps

--- controllers/user-orders.js
+++ controllers/user-orders.js
@@ -7,30 +7,30 @@ const router = express.Router();
 
 // Create a user with the info below.
 router.post('/create/', async (req, res) => {
-    // Start the function
-    User.create(req.body.user).then(createdUser => {
-        Order.create(req.body.order).then(createdOrder => {
-            const OrderContent = require(`../db/models/${createdOrder.contentSchema}.js`);
-            OrderContent.create(req.body.order).then(async createdOrderContent => {
-                Chain.findOne({ name: createdOrder.chainName })
-                    .then(async updatedChain => {
-                        updatedChain.orders.push(createdOrder._id);
-                        createdOrder.chain.push(updatedChain._id);
-                        updatedChain.save();
-                    })
-                    .then(async () => {
-                        createdUser.orders.push(createdOrder._id);
-                        createdOrder.orderContent.push(createdOrderContent._id);
-                        createdOrder.users.push(createdUser._id);
+	// Start the function
+	User.create(req.body.user).then(createdUser => {
+		Order.create(req.body.order).then(createdOrder => {
+			const OrderContent = require(`../db/models/${createdOrder.contentSchema}.js`);
+			OrderContent.create(req.body.order).then(async createdOrderContent => {
+				Chain.findOne({ name: createdOrder.chainName })
+					.then(async updatedChain => {
+						updatedChain.orders.push(createdOrder._id);
+						createdOrder.chain.push(updatedChain._id);
+						updatedChain.save();
+					})
+					.then(async () => {
+						createdUser.orders.push(createdOrder._id);
+						createdOrder.orderContent.push(createdOrderContent._id);
+						createdOrder.users.push(createdUser._id);
 
-                        createdUser.save();
-                        createdOrder.save();
-                        createdOrderContent.save();
-                    })
-                    .then(() => res.json(createdOrder));
-            });
-        });
-    });
+						createdUser.save();
+						createdOrder.save();
+						createdOrderContent.save();
+					})
+					.then(() => res.json(createdOrder));
+			});
+		});
+	});
 });
 
 module.exports = router;
