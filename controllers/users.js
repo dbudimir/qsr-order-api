5c829f6771b1e68b0802b0d10c841418308b2d0e
dbudimir <dav.budimir@gmail.com>
2019-07-16 10:30:09 -0400

    Added unique validation to user fields.

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const jwt = require('jwt-simple')
-const passport = require('../config/passport')
-const config = require('../config/config')
+const jwt = require('jwt-simple');
+const passport = require('../config/passport');
+const config = require('../config/config');
 
 const User = require('../db/models/User.js');
 
@@ -9,98 +9,95 @@ const router = express.Router();
 
 // Gets a list of users
 router.get('/', (req, res) => {
-    User.find({}).then(allUsers => res.json(allUsers));
+	User.find({}).then(allUsers => res.json(allUsers));
 });
 
 // Gets a list of users with populated order information.
 router.get('/all', (req, res) => {
-    User.find({})
-        .deepPopulate(['orders'])
-        .then(allUsers => res.json(allUsers));
+	User.find({})
+		.deepPopulate(['orders'])
+		.then(allUsers => res.json(allUsers));
 });
 
 // Gets a specific user by ID with orders
 router.get('/:id', async (req, res) => {
-    User.find({ _id: req.params.id })
-        .populate({ path: 'orders', refPath: 'chainSchema', populate: { path: 'orderContent' } })
-        .then(allUsers => res.json(allUsers));
+	User.find({ _id: req.params.id })
+		.populate({ path: 'orders', refPath: 'chainSchema', populate: { path: 'orderContent' } })
+		.then(allUsers => res.json(allUsers));
 });
 
-// Create a usr with the info below.
+// Create a user with the info below.
 router.post('/create/:userFullName', (req, res) => {
-    const newUser = JSON.parse(`{ "userFullName":"${req.params.userFullName}" }`);
-    User.create(newUser).then(created => {
-        res.json(created);
-    });
+	const newUser = JSON.parse(`{ "userFullName":"${req.params.userFullName}" }`);
+	User.create(newUser).then(created => {
+		res.json(created);
+	});
 });
 
-// Creates a new user 
+// Creates a new user
 router.post('/signup', (req, res) => {
-    if (req.body.email && req.body.password) {
-        let newUser = {
-            email: req.body.email,
-            password: req.body.password
-        }
-        User.findOne({ email: req.body.email })
-            .then((user) => {
-                if (!user) {
-                    User.create(newUser)
-                        .then(user => {
-                            if (user) {
-                                var payload = {
-                                    id: newUser.id
-                                }
-                                var token = jwt.encode(payload, config.jwtSecret)
-                                res.json({
-                                    token: token,
-                                    userId: user._id
-                                })
-
-                            } else {
-                                res.sendStatus(401)
-                            }
-                            res.json(user)
-                        })
-                } else {
-                    res.sendStatus(401)
-                }
-            })
-    } else {
-        res.sendStatus(401)
-    }
-})
+	if (req.body.email && req.body.password) {
+		let newUser = {
+			email: req.body.email,
+			password: req.body.password,
+		};
+		User.findOne({ email: req.body.email }).then(user => {
+			if (!user) {
+				User.create(newUser).then(user => {
+					if (user) {
+						var payload = {
+							id: newUser.id,
+						};
+						var token = jwt.encode(payload, config.jwtSecret);
+						res.json({
+							token: token,
+							userId: user._id,
+						});
+					} else {
+						res.sendStatus(401);
+					}
+					res.json(user);
+				});
+			} else {
+				res.sendStatus(401);
+			}
+		});
+	} else {
+		res.sendStatus(401);
+	}
+});
 
 // Allows an existing user to log in
 router.post('/login', (req, res) => {
-    if (req.body.email && req.body.password) {
-        User.findOne({ email: req.body.email }).then(user => {
-            if (user) {
-                if (user.password === req.body.password) {
-                    var payload = {
-                        id: user.id
-                    }
-                    var token = jwt.encode(payload, config.jwtSecret)
-                    res.json({
-                        token: token,
-                        userId: user._id
-                    })
-                } else {
-                    res.sendStatus(401)
-                }
-            } else {
-                res.sendStatus(401)
-            }
-        })
-    } else {
-        res.sendStatus(401)
-    }
-})
+	if (req.body.email && req.body.password) {
+		User.findOne({ email: req.body.email }).then(user => {
+			if (user) {
+				if (user.password === req.body.password) {
+					var payload = {
+						id: user.id,
+					};
+					var token = jwt.encode(payload, config.jwtSecret);
+					res.json({
+						token: token,
+						userId: user._id,
+					});
+				} else {
+					res.sendStatus(401);
+				}
+			} else {
+				res.sendStatus(401);
+			}
+		});
+	} else {
+		res.sendStatus(401);
+	}
+});
 
 // Delete a user by name
 router.delete('/delete/:userFullName', (req, res) => {
-    User.findOneAndDelete({ userFullName: req.params.userFullName }).then(deleted => {
-        res.json(deleted);
-    });
+	User.findOneAndDelete({ userFullName: req.params.userFullName }).then(deleted => {
+		res.json(deleted);
+	});
 });
 
 module.exports = router;
--- db/models/User.js
+++ db/models/User.js
@@ -1,16 +1,17 @@
 const mongoose = require('../connection.js');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = new mongoose.Schema({
-    userFullName: { type: String },
-    userName: { type: String, index: { unique: true } },
-    email: { type: String },
-    password: { type: String },
-    orders: [
-        {
-            ref: 'Order',
-            type: mongoose.Schema.Types.ObjectId,
-        },
-    ],
+	userFullName: { type: String },
+	userName: { type: String, unique: true, index: { unique: true } },
+	email: { type: String, unique: true },
+	password: { type: String },
+	orders: [
+		{
+			ref: 'Order',
+			type: mongoose.Schema.Types.ObjectId,
+		},
+	],
 });
 
 const deepPopulate = require('mongoose-deep-populate')(mongoose);
